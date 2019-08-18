fab9dc4e758b72d447fdac57b6c461ce5ff3245f
xywang68 <xywang68@gmail.com>
2019-08-18 10:18:17 -0700

    add docker run method

---
+++ docker/autobdd-run.startup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USER=${USER:-root}
+HOME=/root
+if [ "$USER" != "root" ]; then
+  echo "* enable custom user: $USER"
+  useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+  if [ -z "$PASSWORD" ]; then
+    echo "  set default password to \"ubuntu\""
+    PASSWORD=ubuntu
+  fi
+  HOME=/home/$USER
+  echo "$USER:$PASSWORD" | chpasswd
+  unset PASSWORD
+fi
+
+# home folder
+cd /root && tar cf - ./Projects | (cd $HOME && tar xf -)
+chown -R $USER:$USER $HOME
+
+# set bash_profile
+cat /root/.bashrc >> $HOME/.bash_profile && chown $USER:$USER $HOME/.bash_profile
+cat >> $HOME/.bash_profile << END_bash_profile
+npm config set script-shell /bin/bash
+END_bash_profile
+
+# run test by taking autorun.py parameters
+sudo -E su $USER -m -c " \
+  cd $HOME/Projects/AutoBDD && \
+  . .autoPathrc.sh && \
+  cd test-projects/webtest-example && \
+  npm install && npm start && cd - && \
+  ./framework/scripts/autorunner.py --project webtest-example $@"
+
