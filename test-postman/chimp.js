6db040cb6a4b19346222658a0b1e8b2eb1dc5774
Xinyu Wang <xwang@varmour.com>
2019-09-11 12:06:12 -0700

    re-arrange modules, update chimp.js

--- test-postman/chimp.js
+++ test-postman/chimp.js
@@ -1,4 +1,8 @@
-require('../project/support/env.js');
+process.env.PROJECTBASE = process.env.PROJECTBASE || 'test-projects';
+process.env.PROJECTNAME = process.env.PROJECTNAME || process.env.PWD.split(process.env.PROJECTBASE)[1].split('/')[1]
+var moduleDepth = process.env.PWD.split(process.env.PROJECTNAME)[1].split('/').length - 1
+var relativePathToProject = '../'.repeat(moduleDepth)
+require(relativePathToProject + 'project/support/env.js');
 const projectFullPath = process.env.FrameworkPath + '/test-projects/' + process.env.ThisProject;
 
 var moduleChimp = require(projectFullPath + '/project/support/chimp.js');
