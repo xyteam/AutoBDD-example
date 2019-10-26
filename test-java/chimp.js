30a3ada270ae26dbdfd45ff4031e76f16d7b72f2
xywang68 <xywang68@gmail.com>
2019-10-26 11:17:08 -0700

    delete test-java suite

--- test-java/chimp.js
+++
@@ -1,5 +0,0 @@
-require('./support/env.js')
-var moduleChimp = require(process.env.PROJECTRUNPATH + '/project/support/chimp.js');
-// modify or add myChimp attributes as necessary
-
-module.exports = moduleChimp; 
