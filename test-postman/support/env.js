d7f1a317c5a89d6f7aa11263f5a9b624eb6237a2
xywang68 <xywang68@gmail.com>
2020-04-03 12:56:55 -0700

    update framework env.js

--- test-postman/support/env.js
+++ test-postman/support/env.js
@@ -6,3 +6,4 @@ var relativePathToProject = '../'.repeat(moduleDepth)
 require(relativePathToProject + 'project/support/env.js');
 // define module level Env vars here
 process.env.ThisModule = path.resolve().split(PROJECTNAME)[1].split('/')[1]
+// test env vars here
